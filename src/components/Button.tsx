455b2bcb32a6020a1aae98a5e6c28ae026b9d45a
LuizMaschietto <85773422+LuizMaschietto@users.noreply.github.com>
2021-11-20 14:21:52 -0300

    Fiz uma mudança
    que eu fiz

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,6 +4,9 @@ import '../styles/button.scss'
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
+// essa foi a mudança que eu fiz
+
+
 export function Button(props: ButtonProps){
     return(
         <button className="button" {...props}>
