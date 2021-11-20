86d887ec92039ca5fa4c804de16717804030f64e
LuizMaschietto <85773422+LuizMaschietto@users.noreply.github.com>
2021-11-20 14:24:14 -0300

    parte 2
    que eu fiz

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ import '../styles/button.scss'
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
-// essa foi a mudança que eu fiz
+// essa foi a mudança que eu fiz parte 2
 
 
 export function Button(props: ButtonProps){
